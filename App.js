d87ac1e0a823b20efd9a341392a611bfce8eb191
Kevin Suen <kevinsuen11@gmail.com>
2019-11-27 17:11:55 -0500

    Update mock data

--- App.js
+++ App.js
@@ -2,17 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ItineraryScreen from './src/screens/ItineraryScreen';
 
-const event = {
-  title: "Calgary Tech Conference",
-  startDate: "July 25 · 8:30",
-  endDate: "July 29 · 8:30",
-  departure: "Toronto, ON",
-  arrival: "Calgary, AB",
-  persons: 1,
-  rooms: 1,
-  nights: 4,
-  days: 4
-}
+import event from './mockdata.json';
 
 export default function App() {
   return (
