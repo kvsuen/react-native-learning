f4977828339bf969b83351a792e170c985aa6a11
Kevin Suen <kevinsuen11@gmail.com>
2019-11-26 19:04:26 -0500

    Add header styling

--- src/components/Header/header.component.js
+++ src/components/Header/header.component.js
@@ -1,22 +1,65 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 const Header = ({ title, startDate, endDate, departure, arrival }) => {
   return (
-    <View>
-      <View>
-        <Text>Back Button Placeholder</Text>
+    <View style={styles.container}>
+      <View style={styles.backArrow}>
+        <Ionicons name="md-arrow-back" size={32} color="white" />
       </View>
-      <View>
-        <Text>{title}</Text>
-        <Text>{startDate} {endDate}</Text>
-        <Text>{departure} {arrival}</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.textTitle}>{title}</Text>
+        <Text style={styles.textSubText}>
+          {startDate} &nbsp; / &nbsp; {endDate}
+        </Text>
+        <Text style={styles.textSubText}>
+          {departure}
+          &nbsp; &nbsp;
+          <Ionicons name="ios-swap" size={18} color="white" />
+          &nbsp; &nbsp;
+          {arrival}
+        </Text>
       </View>
-      <View>
-        <Text>Bookmark Placeholder</Text>
+      <View style={styles.bookmark}>
+        <MaterialIcons name="bookmark-border" size={32} color="white" />
       </View>
     </View>
   );
 };
 
 export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginTop: 50,
+    marginBottom: 25
+  },
+  backArrow: {
+    marginHorizontal: 25
+  },
+  textContainer: {
+    flex: 1
+  },
+  textTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 2
+  },
+  textSubText: {
+    color: 'white',
+    fontSize: 18,
+    marginVertical: 2
+  },
+  bookmark: {
+    backgroundColor: 'rgb(255, 198, 112)',
+    marginHorizontal: 25,
+    borderRadius: 100,
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
--- src/screens/ItineraryScreen.js
+++ src/screens/ItineraryScreen.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import Header from '../components/Header/header.component';
 import TravelTab from '../components/TravelTab/travel-tab.component';
 
 const ItineraryScreen = ({ event }) => {
   return (
-    <View>
-      <Header
-        title={event.title}
-        startDate={event.startDate}
-        endDate={event.endDate}
-        departure={event.departure}
-        arrival={event.arrival}
-      />
-      <TravelTab type="flight"/>
-      <TravelTab type="hotel"/>
-      <TravelTab type="car"/>
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#4B99A0', '#57C6C3']}
+        style={{ flex: 1 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <Header
+          title={event.title}
+          startDate={event.startDate}
+          endDate={event.endDate}
+          departure={event.departure}
+          arrival={event.arrival}
+        />
+        <TravelTab
+          type="flight"
+          persons={event.persons}
+          departure={event.departure}
+          arrival={event.arrival}
+        />
+        <TravelTab
+          type="hotel"
+          rooms={event.rooms}
+          nights={event.nights}
+          arrival={event.arrival}
+        />
+        <TravelTab type="car" days={event.days} arrival={event.arrival} />
+      </LinearGradient>
     </View>
   );
 };
 
 export default ItineraryScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
