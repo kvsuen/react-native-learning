de580bd522f8ce5e367d6610b07e143ace0d7379
Kevin Suen <kevinsuen11@gmail.com>
2019-11-26 23:37:10 -0500

    Add trip overview footer

---
+++ src/components/TripOverviewFooter/trip-overview-footer.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const TripOverviewFooter = ({ price }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Trip Overview</Text>
+        <Text style={styles.price}>C$ 1000</Text>
+      </View>
+    </View>
+  );
+}
+ 
+export default TripOverviewFooter;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 0.2,
+    borderTopLeftRadius: 35,
+    paddingLeft: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    marginVertical: 15,
+    alignItems: 'center',
+  },
+  title: {
+    flex: 1,
+    fontSize: 16,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    marginHorizontal: 15
+  },
+  price: {
+    fontSize: 20,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    marginRight: 35
+  },
+});
